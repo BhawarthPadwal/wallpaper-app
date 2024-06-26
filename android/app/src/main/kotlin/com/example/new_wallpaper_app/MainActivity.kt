3c7b8af66427351f8e6391d506e0f140948c67a8
BhawarthPadwal <bhawarthpadwal7@gmail.com>
2024-06-26 15:51:37 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/new_wallpaper_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_wallpaper_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:new_wallpaper_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wallpaper App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
